154e750b97ed4b770b585cfab8e23e3de63bfdc1
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-04-05 15:14:58 +0530

    Conditions & Loops

---
+++ ex33.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def myFunc(n):
+	i = 0
+	numbers =[]
+
+	while i < n:
+		print ("At the top i is %d" % i)
+		numbers.append(i)
+		
+		i = i + 1
+		print ("Numbers now: ", numbers)
+		print ("At the bottom i is %d" % i)
+		
+	print ("The numbers:")
+	for num in numbers:
+		print (num)
+
+myFunc(n)
